9e5f61c9408c2f163fab57f10760f0a8354948ef
rericha19 <samuel.rericha@gmail.com>
2022-03-19 17:15:52 +0100

    tweak

--- texthem-frontend/src/store/mainStore/mutations.ts
+++ texthem-frontend/src/store/mainStore/mutations.ts
@@ -2,14 +2,14 @@ import { MutationTree } from 'vuex';
 import { GlobalStateInterface } from './state';
 
 const mutation: MutationTree<GlobalStateInterface> = {
-    toggleLeftDrawer(state: GlobalStateInterface) {
-        state.leftDrawerState = !state.leftDrawerState;
+    toggleLeftDrawer(state: GlobalStateInterface, value: boolean) {
+        state.leftDrawerState = value
     },
     toggleRightDrawer(state: GlobalStateInterface, value: boolean) {
         state.rightDrawerState = value;
     },
-    toggleMobileNavbar(state: GlobalStateInterface) {
-        state.mobileNavbarState = !state.mobileNavbarState;
+    toggleMobileNavbar(state: GlobalStateInterface, value: boolean) {
+        state.mobileNavbarState = value
     },
 };
 
